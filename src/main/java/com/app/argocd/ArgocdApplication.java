eeaa596a077237c0424b178459127d23eb5c265f
syjoy1993 <syjoy1993@gmail.com>
2025-01-16 09:27:40 +0900

    메vmfhrmfoald세지

---
+++ src/main/java/com/app/argocd/ArgocdApplication.java
@@ -0,0 +1,13 @@
+package com.app.argocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgocdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgocdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/argocd/ArgocdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.argocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgocdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
